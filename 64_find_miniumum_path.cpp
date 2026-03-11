dbf63beacce086b29138f2e1969fffdcd25e03c4
lizzna <nguyenlynakp@gmail.com>
2026-03-11 09:54:05 +0700

    dptongmin

--- 64_find_miniumum_path.cpp
+++ 64_find_miniumum_path.cpp
@@ -1,8 +1,33 @@
-
-int minPathSum(vector<vector<int>>& grid) {
-    vector<vector<int>>f;
-    for(ỉn i=0;;i<grid.size()){
-        for(int j=0;j<grid[i].size();j++){
-            
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>f(n,vector<int>(m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                f[i][j]=1e9;
+            }
         }
+        f[0][0]=grid[0][0];
+        for( int i=0;i< n ; i++)  {
+            for(int j=0;j<m;j++){
+               if (i == 0 && j == 0) {
+                    f[i][j] = grid[i][j]; 
+                } 
+                else if (i == 0) { 
+                    f[i][j] = f[i][j-1] + grid[i][j];
+                } 
+                else if (j == 0) { 
+                    f[i][j] = f[i-1][j] + grid[i][j];
+                } 
+                else {
+                    f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j];
+                }
+                
+            }
+        }
+         return f[n-1][m-1];
+        
     }
+};
